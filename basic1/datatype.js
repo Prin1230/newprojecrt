2ffbbed2b617d6075a6af4dd20ca9e04a0aaf377
Prin1230 <107100397+Prin1230@users.noreply.github.com>
2024-02-17 06:06:06 +0000

    datatype

---
+++ basic1/datatype.js
@@ -0,0 +1,17 @@
+let name="prince" //RANGE 2 TO POWER 53
+let age=98 //BIGINT
+let isLoggedIn=false 
+let state;
+console.log(typeof age );
+    
+    
+    
+    //NULL=STANDLONE VALUE
+    //undefined value not intilize
+    //symbol=>unique
+    //object
+    
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
+       
+   
