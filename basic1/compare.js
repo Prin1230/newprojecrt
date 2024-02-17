e061178a95132c89447b0f4d77a92059996f78b1
Prin1230 <107100397+Prin1230@users.noreply.github.com>
2024-02-17 06:20:45 +0000

    compare

---
+++ basic1/compare.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1); //datatype should be same
+console.log(null<=0)
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+ //null in comparision is treated as 0
+
+ console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//=== its check datatype also
+console.log("2" === 2);
--- basic1/datatype1.js
+++ basic1/datatype1.js
@@ -1,10 +1,16 @@
 let score = "hitesh"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
 
 let isLoggedIn = "hitesh"
 
@@ -20,9 +26,43 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
-//console.log(valueInNumber);
 
+// *********************** Operations ***********************
 
-// "33" => 33
-// "33abc" => NaN
-//  true => 1; false => 0
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //if string first then all convert into string 
+// console.log(1 + 2 + "2");//if string last first operation done then adding like string
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
